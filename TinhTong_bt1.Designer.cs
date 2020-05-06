6730604453095473a19323ea79b6a55e573fed4b
Trần Thị Khuy <64148901+khuytran2000@users.noreply.github.com>
2020-05-06 23:14:01 +0700

    Add files via upload

---
+++ TinhTong_bt1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Chuong4_part1
+{
+    partial class TinhTong_bt1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtA = new System.Windows.Forms.Label();
+            this.txtB = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnTong = new System.Windows.Forms.Button();
+            this.btnLamLai = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.lbTong = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtA
+            // 
+            this.txtA.AutoSize = true;
+            this.txtA.Location = new System.Drawing.Point(74, 35);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(45, 13);
+            this.txtA.TabIndex = 0;
+            this.txtA.Text = "Nhập a:";
+            // 
+            // txtB
+            // 
+            this.txtB.AutoSize = true;
+            this.txtB.Location = new System.Drawing.Point(74, 85);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(45, 13);
+            this.txtB.TabIndex = 1;
+            this.txtB.Text = "Nhập b:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(145, 82);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(145, 35);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // btnTong
+            // 
+            this.btnTong.Location = new System.Drawing.Point(54, 171);
+            this.btnTong.Name = "btnTong";
+            this.btnTong.Size = new System.Drawing.Size(75, 23);
+            this.btnTong.TabIndex = 4;
+            this.btnTong.Text = "Tổng";
+            this.btnTong.UseVisualStyleBackColor = true;
+            this.btnTong.Click += new System.EventHandler(this.btnTong_Click);
+            // 
+            // btnLamLai
+            // 
+            this.btnLamLai.AllowDrop = true;
+            this.btnLamLai.Location = new System.Drawing.Point(170, 171);
+            this.btnLamLai.Name = "btnLamLai";
+            this.btnLamLai.Size = new System.Drawing.Size(75, 23);
+            this.btnLamLai.TabIndex = 5;
+            this.btnLamLai.Text = "Làm lại";
+            this.btnLamLai.UseVisualStyleBackColor = true;
+            this.btnLamLai.Click += new System.EventHandler(this.btnLamLai_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(278, 171);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 6;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // lbTong
+            // 
+            this.lbTong.AutoSize = true;
+            this.lbTong.Location = new System.Drawing.Point(74, 133);
+            this.lbTong.Name = "lbTong";
+            this.lbTong.Size = new System.Drawing.Size(0, 13);
+            this.lbTong.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(74, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(74, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(74, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 10;
+            // 
+            // TinhTong_bt1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 221);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbTong);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnLamLai);
+            this.Controls.Add(this.btnTong);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Name = "TinhTong_bt1";
+            this.Text = "TinhTong_bt1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtA;
+        private System.Windows.Forms.Label txtB;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnTong;
+        private System.Windows.Forms.Button btnLamLai;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Label lbTong;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ TinhTong_bt1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chuong4_part1
+{
+    public partial class TinhTong_bt1 : Form
+    {
+        public TinhTong_bt1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (((e.KeyChar >= '0') && (e.KeyChar <= '9')) ||
+               (e.KeyChar == '-') || (Convert.ToInt32(e.KeyChar) == 8) || (Convert.ToInt32(e.KeyChar) == 13))
+                e.Handled = false;
+            else e.Handled = true;
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (((e.KeyChar >= '0') && (e.KeyChar <= '9')) ||
+               (e.KeyChar == '-') || (Convert.ToInt32(e.KeyChar) == 8) || (Convert.ToInt32(e.KeyChar) == 13))
+                e.Handled = false;
+            else e.Handled = true;
+        }
+
+        private void btnTong_Click(object sender, EventArgs e)
+        {
+            if (txtA.Text == "")
+            {
+                MessageBox.Show("Bạn chưa nhập a");
+                txtA.Focus();
+                return;
+            }
+            if (txtB.Text == "")
+            {
+                MessageBox.Show("Bạn chưa nhập b");
+                txtB.Focus();
+                return;
+            }
+
+            int tong = 0;
+            int a = Convert.ToInt32(txtA.Text);
+            int b = Convert.ToInt32(txtB.Text);
+
+            
+            if (a < b)
+            {
+
+                for (int i = a; i < b; i++)
+                {
+                    tong += i;
+                }
+                 label1.Text = tong.ToString();
+            }
+            else
+            {
+                for(int i = b; i < a; i++)
+                {
+                    tong = tong + i;
+                }
+                label1.Text = tong.ToString();
+            }
+           
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+   
+
+        private void btnLamLai_Click(object sender, EventArgs e)
+        {
+            txtA.Text = "";
+            txtB.Text = "";
+            label1.Text = "";
+            txtA.Focus();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
